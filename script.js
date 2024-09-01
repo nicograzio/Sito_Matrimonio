54f35fadf4a6ebce010297d7826bf9a38dab9e32
nicograzio <nicholasgrazioli@hotmail.it>
2024-09-01 17:22:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Imposta la data del matrimonio
 var weddingDate = new Date("Sep 13, 2025 00:00:00").getTime();
 var countdown = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = weddingDate - now;
+	var now = new Date().getTime();
+	var distance = weddingDate - now;
 	if (distance > 0) {
 		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -14,12 +14,12 @@ var countdown = setInterval(function() {
 			document.getElementById("time").innerHTML = "Wedding Countdown: <br>" + days + "g " + hours + "h " + minutes + "m " + seconds + "s ";
 	}
 	else if (distance <= 0 && distance > -86400000) {
-        clearInterval(countdown);
-        document.getElementById("time").innerHTML = "È il giorno del matrimonio!";
-    }
+		clearInterval(countdown);
+		document.getElementById("time").innerHTML = "È il giorno del matrimonio!";
+	}
 	else {
 		clearInterval(countdown);
-        document.getElementById("time").innerHTML = "Ci siamo SPOSATI!";
+		document.getElementById("time").innerHTML = "Ci siamo SPOSATI!";
 	}
 }, 1000);
 
@@ -29,28 +29,28 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('nav ul li a');
 window.addEventListener('scroll', function() {
 	// Effetto Parallax su immagini di sfondo
-    let win_pos = window.pageYOffset;
-    contents.forEach((content) => {
-        let content_move = content.getBoundingClientRect().top;
-        if (win_pos > content_move) {
-            content.style.transform = "translateY(" + content_move * 0.15 + "px";
-        }
-    });
+	let win_pos = window.pageYOffset;
+	contents.forEach((content) => {
+		let content_move = content.getBoundingClientRect().top;
+		if (win_pos > content_move) {
+		    content.style.transform = "translateY(" + content_move * 0.15 + "px";
+		}
+	});
 
 	// Selezione del link nella navbar in base alla sezione
-    sections.forEach((section, index) => {
-        let rect = section.getBoundingClientRect();
-        if (rect.top <= 70 && rect.bottom >= 70) {
-            navLinks.forEach(link => link.classList.remove('active'));
-            navLinks[index].classList.add('active');
-        }
-    });
+	sections.forEach((section, index) => {
+		let rect = section.getBoundingClientRect();
+		if (rect.top <= 70 && rect.bottom >= 70) {
+		    navLinks.forEach(link => link.classList.remove('active'));
+		    navLinks[index].classList.add('active');
+		}
+	});
 });
 
 // Mostra/nasconde il menu di navigazione al clic del menu hamburger
 document.querySelector('.menu-toggle').addEventListener('click', function() {
-    this.classList.toggle('active');
-    document.querySelector('nav ul').classList.toggle('show');
+	this.classList.toggle('active');
+	document.querySelector('nav ul').classList.toggle('show');
 });
 
 // Autoplay video
@@ -59,19 +59,18 @@ video.play();
 
 // Invio dati del form per la prenotazione
 document.getElementById('Prenotazione').addEventListener('submit', async function(e) {
-  e.preventDefault(); // Previene il comportamento predefinito di invio del form
-  const formData = new FormData(this); // Cattura i dati del form
-  const response = await fetch('https://matrimonio-nicholas-e-carlotta.netlify.app/.netlify/functions/submit', {
-    method: 'POST',
-    body: JSON.stringify(Object.fromEntries(formData)),
-    headers: { 'Content-Type': 'application/json' }
-  });
-
-  if (response.ok) {
-    alert('Grazie per aver inoltrato la tua partecipazione!');
-  } else {
-    alert('Si è verificato un errore, ti preghiamo di riprovare più tardi.');
-  }
+	e.preventDefault(); // Previene il comportamento predefinito di invio del form
+	const formData = new FormData(this); // Cattura i dati del form
+	const response = await fetch('https://matrimonio-nicholas-e-carlotta.netlify.app/.netlify/functions/submit', {
+		method: 'POST',
+		body: JSON.stringify(Object.fromEntries(formData)),
+		headers: { 'Content-Type': 'application/json' }
+	});
+	
+	if (response.ok)
+		alert('Grazie per aver inoltrato la tua partecipazione!');
+	else
+		alert('Si è verificato un errore, ti preghiamo di riprovare più tardi.');
 });
 
 // Invio delle foto dal form nella gallery
@@ -90,24 +89,26 @@ document.getElementById('upload-form').addEventListener('submit', async function
 	}
 	try {
 	const response = await fetch('https://matrimonio-nicholas-e-carlotta.netlify.app/.netlify/functions/upload-to-github', {
-	    method: 'POST',
-	    body: JSON.stringify({ files: fileArray }),
-	    headers: { 'Content-Type': 'application/json' }
+		method: 'POST',
+		body: JSON.stringify({ files: fileArray }),
+		headers: { 'Content-Type': 'application/json' }
 	});
 	
 	const result = await response.json();
+	alert('Caricamento avvenuto con successo!');
 	document.getElementById('upload-status').innerText = result.message;
 	} catch (error) {
-	  document.getElementById('upload-status').innerText = 'Errore nel caricamento su GitHub.';
-	  console.error('Errore:', error);
+		alert('Errore nel caricamento! Ti preghiamo di riprovare più tardi.\nNel caso il problema persista puoi contattare Nicholas.');
+		document.getElementById('upload-status').innerText = 'Errore nel caricamento delle foto';
+		console.error('Errore:', error);
 	}
 
 	function toBase64(file) {
-	    return new Promise((resolve, reject) => {
-	        const reader = new FileReader();
-	        reader.readAsDataURL(file);
-	        reader.onload = () => resolve(reader.result.split(',')[1]);
-	        reader.onerror = error => reject(error);
-	    });
+	return new Promise((resolve, reject) => {
+		const reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onload = () => resolve(reader.result.split(',')[1]);
+		reader.onerror = error => reject(error);
+	});
 	}
 });
