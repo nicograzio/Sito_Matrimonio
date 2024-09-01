1e01673bb08f5c84999c828acbc2103f46823178
nicograzio <nicholasgrazioli@hotmail.it>
2024-09-01 12:30:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ var video = document.getElementById('bg-video');
 video.play();
 
 // Invio dati del form per la prenotazione
-/*document.getElementById('Prenotazione').addEventListener('submit', async function(e) {
+document.getElementById('Prenotazione').addEventListener('submit', async function(e) {
   e.preventDefault(); // Previene il comportamento predefinito di invio del form
   const formData = new FormData(this); // Cattura i dati del form
   const response = await fetch('https://matrimonio-nicholas-e-carlotta.netlify.app/.netlify/functions/submit', {
@@ -72,36 +72,9 @@ video.play();
   } else {
     alert('Si è verificato un errore, ti preghiamo di riprovare più tardi.');
   }
-});*/
+});
 
 // Invio delle foto dal form nella gallery
-/*document.getElementById('upload-form').addEventListener('submit', async function(event) {
-	event.preventDefault();
-	console.log('Modulo inviato');
-	const fileInput = document.getElementById('file');
-	const file = fileInput.files[0];
-	const reader = new FileReader();
-	
-	reader.onload = async function() {
-	const fileContent = reader.result.split(',')[1]; // Ottieni il contenuto del file in Base64
-	try {
-	  const response = await fetch('https://matrimonio-nicholas-e-carlotta.netlify.app/.netlify/functions/upload-to-github', {
-	    method: 'POST',
-	    body: JSON.stringify({
-	      fileName: file.name,
-	      fileContent: fileContent,
-	    }),
-	    headers: { 'Content-Type': 'application/json' }
-	  });
-	console.log('Risposta del server:', response);
-	
-	  const result = await response.json();
-	  document.getElementById('upload-status').innerText = result.message;
-	} catch (error) {
-	  document.getElementById('upload-status').innerText = 'Errore nel caricamento su GitHub.';
-	  console.error('Errore:', error);
-	}
-};*/
 document.getElementById('upload-form').addEventListener('submit', async function(event) {
 	event.preventDefault();
 	console.log('Modulo inviato');
