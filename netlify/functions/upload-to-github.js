2ee5575b9e559759a7ecdd50f88bbe2024debdb3
nicograzio <nicholasgrazioli@hotmail.it>
2024-09-01 11:51:11 +0200

    Update upload-to-github.js

--- netlify/functions/upload-to-github.js
+++ netlify/functions/upload-to-github.js
@@ -1,7 +1,8 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
-  console.log('Funzione invocatacon metodo HTTP:', event.httpMethod);
+  console.log('Funzione invocata con metodo HTTP:', event.httpMethod);
+  
   if (event.httpMethod !== 'POST') {
     console.log('Metodo non consentito');
     return {
@@ -12,31 +13,33 @@ exports.handler = async (event, context) => {
 
   const GITHUB_TOKEN = process.env.GITHUB_TOKEN; // Usa le variabili di ambiente per sicurezza
   console.log('Token ottenuto:', GITHUB_TOKEN ? 'Si' : 'No');
+
   const { fileContent, fileName } = JSON.parse(event.body); // Recupera il contenuto e il nome del file dal body della richiesta
   const repoOwner = 'nicograzio'; // Inserisci il tuo nome utente GitHub
   const repoName = 'Sito_Matrimonio'; // Inserisci il nome del repository
-  const filePath = 'images/${fileName}'; // Specifica il percorso del file nel repo
+  const filePath = `images/${fileName}`; // Specifica il percorso del file nel repo
 
-  const githubApiUrl = 'https://api.github.com/repos/${repoOwner}/${repoName}/${filePath}';
+  const githubApiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`;
 
   const requestBody = {
     message: 'Aggiunto nuovo file',  // Messaggio di commit per il caricamento del file
-    content: Buffer.from(fileContent, 'base64').toString(),  // Codifica il contenuto del file in base64
+    content: Buffer.from(fileContent, 'base64').toString('base64'),  // Codifica il contenuto del file in base64
   };
 
   try {
     const response = await fetch(githubApiUrl, {
       method: 'PUT',
       headers: {
-        'Authorization': token ${GITHUB_TOKEN},  // Usa il token GitHub per autorizzare la richiesta
+        'Authorization': `token ${GITHUB_TOKEN}`,  // Usa il token GitHub per autorizzare la richiesta
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(requestBody),  // Invia il corpo della richiesta con i dettagli del commit
     });
-    console.log('Risposta da Github:', response.status);
+    
+    console.log('Risposta da GitHub:', response.status);
 
     if (!response.ok) {
-      throw new Error('Errore nel caricamento del file: ${response.statusText}');
+      throw new Error(`Errore nel caricamento del file: ${response.statusText}`);
     }
 
     return {
