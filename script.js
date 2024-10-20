8330767da5b6784402386e6c9e7477f7a04cd418
nicograzio <nicholasgrazioli@hotmail.it>
2024-10-20 12:26:42 +0200

    error in the cookie management

--- script.js
+++ script.js
@@ -597,7 +597,7 @@ function setCookie(name, value, days) {
         date.setTime(date.getTime() + (days*24*60*60*1000));
         expires = "; expires=" + date.toUTCString();
     }
-	document.cookie = name + "=" + encodeURIComponent(value || "") + expires + "; path=/; SameSite=None; Secure"";
+	document.cookie = name + "=" + encodeURIComponent(value || "") + expires + "; path=/; SameSite=None; Secure";
 }
 
 function getCookie(name) {
